9c1a81b70664c049d8bafb55318cdd463a5a6e60
Natalia Kals <nataliakals03@gmail.com>
2024-08-05 15:55:58 +1200

    conecting databse with userController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,23 @@
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+const { hash } = require("bcryptjs");
 
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
-        if (!name) {
-            throw new AppError("O nome nao foi informado");
+        const database = await sqliteConnection();
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if (checkUserExists) {
+            throw new AppError("Este email ja esta em uso");
         }
 
-        response.json({ name, email, password })
+        const hashedPassword = await hash(password, 8);
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+
+        return response.status(201).json();
     }
 }
 
