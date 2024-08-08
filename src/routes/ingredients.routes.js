65d539eff6b446048f279458d4b3dd0ec816912e
Natalia Kals <nataliakals03@gmail.com>
2024-08-09 09:41:58 +1200

    creating json token

--- src/routes/ingredients.routes.js
+++ src/routes/ingredients.routes.js
@@ -4,8 +4,6 @@ const IngredientsController = require("../controllers/IngredientsController");
 const ingredientsRoutes = Router();
 const ingredientsController = new IngredientsController();
 
-// ingredientsRoutes.post("/", ingredientsController.create);
-// ingredientsRoutes.get("/:dish_id", ingredientsController.getOne);
 ingredientsRoutes.get("/", ingredientsController.GetAll);
 
 module.exports = ingredientsRoutes;
